e0b5fc80a39c07045c72e9abe92570bb3f0b4b0c
lirikgod <geimervvt1@gmail.com>
2024-05-17 08:57:52 +0300

    fix error

--- Practice_3-4/pr3_4_variant-1_task-5.sql
+++ Practice_3-4/pr3_4_variant-1_task-5.sql
@@ -3,4 +3,4 @@ DECLARE @TargetEmployeeID INT = 102;
 SELECT DISTINCT s.Фамилия_сотр
 FROM Сотрудники s
 INNER JOIN История_работы h ON s.Номер_сотр = h.Номер_сотр
-WHERE YEAR(h.Дата_приема) = (SELECT YEAR(Дата_приема) FROM История_работы WHERE Номер_сотр = @TargetEmployeeID);
+WHERE YEAR(h.Дата_приема) IN (SELECT YEAR(Дата_приема) FROM История_работы WHERE Номер_сотр = @TargetEmployeeID);
