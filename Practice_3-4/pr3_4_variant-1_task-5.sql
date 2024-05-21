e17e6e2d768ab211378dce087666665a10fbcc67
Revenant <129858973+Revenant121@users.noreply.github.com>
2024-05-21 16:14:14 +0300

    Delete Practice_3-4 directory

--- Practice_3-4/pr3_4_variant-1_task-5.sql
+++
@@ -1,6 +0,0 @@
-DECLARE @TargetEmployeeID INT = 102;
-
-SELECT DISTINCT s.Фамилия_сотр
-FROM Сотрудники s
-INNER JOIN История_работы h ON s.Номер_сотр = h.Номер_сотр
-WHERE YEAR(h.Дата_приема) IN (SELECT YEAR(Дата_приема) FROM История_работы WHERE Номер_сотр = @TargetEmployeeID);
